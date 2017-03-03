f0ca6b5f31b2b0fafa972dcf1f145b4e064bd61b
Andrew Lyu <apemost@gmail.com>
2017-03-03 15:05:59 +0800

    Exclude .gitmodules

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ git pull origin master
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
+		--exclude ".gitmodules" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE" \
