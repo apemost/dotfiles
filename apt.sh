bef9cefbaddc6aa1020f955cce6b6a4a6bf01547
Andrew Lyu <apemost@gmail.com>
2017-03-10 09:58:42 +0800

    Add apt.sh for debian-based system

---
+++ apt.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using apt
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install curl
+sudo apt-get install wget
+
+# Install development tools
+sudo apt-get install git
+sudo apt-get install gnupg2
+sudo apt-get install lua
+sudo apt-get install python
+sudo apt-get install python3
+
+# Install other useful binaries
+sudo apt-get install tmux
