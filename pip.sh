fa4de3d3ded9be4987ab93fc484585da87737ade
Andrew Lyu <apemost@gmail.com>
2017-04-20 16:02:43 +0800

    Update pip.sh and add .travis.yml

--- pip.sh
+++ pip.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-sudo -H pip install --upgrade pip
+function pip_install() {
+  sudo -H pip list --outdated --format=freeze \
+    | grep -v '^\-e' \
+    | cut -d = -f 1  \
+    | xargs -n1 sudo -H pip install --upgrade
 
-sudo -H pip install requests
-sudo -H pip install speedtest-cli
+  sudo -H pip install ipython
+  sudo -H pip install pylint
+  sudo -H pip install requests
+  sudo -H pip install setuptools
+  sudo -H pip install speedtest-cli
+}
+
+function pip3_install() {
+  sudo -H pip3 list --outdated --format=freeze \
+    | grep -v '^\-e' \
+    | cut -d = -f 1  \
+    | xargs -n1 sudo -H pip3 install --upgrade
+
+  sudo -H pip3 install ipython
+  sudo -H pip3 install you-get
+}
+
+which pip &> /dev/null && pip_install
+which pip3 &> /dev/null && pip3_install
