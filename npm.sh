610633be1ee6e0b811e0c0f8883a09846c11b8ac
Andrew Lyu <apemost@gmail.com>
2017-06-05 19:08:34 +0800

    Update .eslintrc and npm.sh

--- npm.sh
+++ npm.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-sudo npm install -g eslint
-sudo npm install -g eslint-config-airbnb
-sudo npm install -g eslint-plugin-import
-sudo npm install -g eslint-plugin-jsx-a11y
-sudo npm install -g eslint-plugin-react
+sudo npm install -g eslint \
+                    eslint-config-airbnb \
+                    eslint-plugin-import \
+                    eslint-plugin-jsx-a11y \
+                    eslint-plugin-react
+
 sudo npm install -g gulp
 sudo npm install -g mocha
 sudo npm install -g stylelint
