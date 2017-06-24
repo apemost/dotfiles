9ece2fd1af0fc8974e4ccad36bb28310fa3e5cd5
Andrew Lyu <andrew.lyu@gllue.com>
2017-06-24 13:43:22 +0800

    Remove npm.sh and pip.sh

--- npm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo npm install -g eslint eslint-config-google
-sudo npm install -g gulp
-sudo npm install -g mocha
-sudo npm install -g stylelint
-sudo npm install -g webpack
