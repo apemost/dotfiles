a94dd1a7c85e9db5c9c926cd8c8a2b48b9e93569
Andrew Lyu <apemost@gmail.com>
2017-06-30 23:00:15 +0800

    Add npm.sh and pip.sh

---
+++ npm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+npm install -g eslint
+npm install -g htmlhint
+npm install -g jsonlint
+npm install -g stylelint
---
+++ pip.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+pip3 install ipython
+pip3 install ipdb
+pip3 install virtualenv
