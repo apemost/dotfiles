1818b7d85656e188cfe9106d3c9b7a980db6b313
Andrew Lyu <apemost@gmail.com>
2017-07-26 02:17:41 +0800

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -8,7 +8,9 @@ sudo apt-get install -y wget
 
 sudo apt-get install -y zsh
 
-chsh -s /usr/bin/zsh
+if [ "$SHELL" != '/usr/bin/zsh' ]; then
+  chsh -s /usr/bin/zsh
+fi
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
