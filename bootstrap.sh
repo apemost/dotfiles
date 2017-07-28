ba50338e99ccfd5e0ff2872f04cd24ebd7275577
Andrew Lyu <apemost@gmail.com>
2017-07-28 22:56:00 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,29 @@
 git pull origin master
 
-rsync_linux() {
-  rsync --exclude ".git/" \
-    --exclude ".DS_Store" \
-    --exclude ".mac" \
-    --exclude ".macos" \
-    --exclude "brew.sh" \
-    --exclude "bootstrap.sh" \
-    --exclude "README.md" \
-    --exclude "LICENSE" \
+if [ "$(uname -s)" = 'Darwin' ]; then
+  rsync \
+    --exclude '.git/' \
+    --exclude '.DS_Store' \
+    --exclude 'apt.sh' \
+    --exclude 'bootstrap.sh' \
+    --exclude 'README.md' \
+    --exclude 'LICENSE' \
     -avh --no-perms . ~
-}
-
-rsync_darwin() {
-  rsync --exclude ".git/" \
-    --exclude ".DS_Store" \
-    --exclude "apt.sh" \
-    --exclude "bootstrap.sh" \
-    --exclude "README.md" \
-    --exclude "LICENSE" \
-    -avh --no-perms . ~
-}
-
-platform="$(uname -s)"
-
-if [ "$platform" = "Linux" ]; then
-  rsync_linux
-elif [ "$platform" = "Darwin" ]; then
-  rsync_darwin
 else
-  echo "My dotfiles are not supported yet on this platform ($platform)."
-  exit 1
+  rsync \
+    --exclude '.git/' \
+    --exclude '.DS_Store' \
+    --exclude '.mac' \
+    --exclude '.macos' \
+    --exclude 'brew.sh' \
+    --exclude 'bootstrap.sh' \
+    --exclude 'README.md' \
+    --exclude 'LICENSE' \
+    -avh --no-perms . ~
 fi
 
-unset rsync_linux
-unset rsync_darwin
-unset platform
-exec "$SHELL" -l
+if [[ "$SHELL" =~ 'zsh$' ]]; then
+  source ~/.zshrc
+else
+  source ~/.bashrc
+fi
