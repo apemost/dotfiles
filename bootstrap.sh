4eda23c27717dc2191072a4e789045728f5c759e
Andrew Lyu <apemost@gmail.com>
2018-05-05 18:01:18 +0800

    Add .travis.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ git pull origin master
 rsync \
   --exclude '.git' \
   --exclude '.DS_Store' \
+  --exclude '.travis.yml' \
   --exclude 'LICENSE' \
   --exclude 'README.md' \
   --exclude 'bootstrap.sh' \
