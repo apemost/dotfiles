dd419a214afdd0ceb53a31033aca7fc215413020
Andrew Lyu <apemost@gmail.com>
2019-12-30 12:55:53 +0800

    Add yapf

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo pip3 install black
 sudo pip3 install docker-compose
 sudo pip3 install grip
 sudo pip3 install mycli
 sudo pip3 install pgcli
 sudo pip3 install tldr
 sudo pip3 install yamllint
+sudo pip3 install yapf
