4b6bafe2fe6f71dd8beefbd22e98e0d3273d88df
Andrew Lyu <apemost@gmail.com>
2026-06-23 19:41:43 +0800

    fix(bootstrap): skip git pull on dirty working tree

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-git pull origin main
+has_uncommitted_changes() {
+  [[ -n "$(git status --porcelain --untracked-files=no 2>/dev/null)" ]]
+}
+
+if has_uncommitted_changes; then
+  echo "Skipped git pull because there are uncommitted changes."
+else
+  git pull origin main
+fi
 
 rsync -avh --no-perms \
   .aliases \
